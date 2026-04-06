fc297e3fbca7a9b99d63799733503e540be5382f
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-05 23:16:10 -0400

    Update whats-new-data.js

--- src/components/whats-new/whats-new-data.js
+++ src/components/whats-new/whats-new-data.js
@@ -1,10 +1,9 @@
-import blockFonts from './blockfonts.gif'
-import firstDay from './firstday.png'
+import blockFonts from './firstday.png'
 
 export default {
     message: "A web was born!",
     author: "By Gaiawindwave90",
-    picture: firstDay,
+    picture: blockFonts,
     avatarUrl: "https://avatars.githubusercontent.com/u/4597024?v=4&size=40",
     secondMessage: "Sorry because I was having a meltdown over something...",
     pictureData: {
