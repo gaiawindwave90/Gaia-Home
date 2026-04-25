37ba463a2a8dc16b86e8b84e3dd138c673a0015c
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-25 05:46:32 -0400

    THE SKIBIDI TOILET NOTE SAVES THE DAY!

--- src/lib/example-cards/index.js
+++ src/lib/example-cards/index.js
@@ -7,22 +7,22 @@ const exampleCards = [
     {
         name: "Whack-A-Vio",
         iconURL: WhackVioThumb,
-        href: "https://gaiamod-main.github.io/?project_url=https://github.com/GaiaMod-Main/examples/raw/refs/heads/main/src/gaia-files/Whack-A-Vio.gaia"
+        href: "https://gaiamod-main.github.io/?project_url=https://raw.githubusercontent.com/GaiaMod-Main/examples/refs/heads/main/src/gaia-files/Whack-A-Vio.gaia"
     },
     {
         name: "Domino and Disco Goes To The Walmart Store",
         iconURL: DominoThumb,
-        href: "https://gaiamod-main.github.io/?project_url=https://github.com/GaiaMod-Main/examples/raw/refs/heads/main/src/gaia-files/Domino%20and%20Disco%20Goes%20To%20The%20Walmart%20Store.gaia"
+        href: "https://gaiamod-main.github.io/?project_url=https://raw.githubusercontent.com/GaiaMod-Main/examples/refs/heads/main/src/gaia-files/Domino%20and%20Disco%20Goes%20To%20The%20Walmart%20Store.gaia"
     },
     {
         name: "Secret Message",
         iconURL: ILoveYouThumb,
-        href: "https://gaiamod-main.github.io/?project_url=https://github.com/GaiaMod-Main/examples/raw/refs/heads/main/src/gaia-files/Someone%20Loves%20You!.gaia"
+        href: "https://gaiamod-main.github.io/?project_url=https://raw.githubusercontent.com/GaiaMod-Main/examples/refs/heads/main/src/gaia-files/Someone%20Loves%20You!.gaia"
     },
     {
         name: "ChatGPT",
         iconURL: ChatGPTThumb,
-        href: "https://gaiamod-main.github.io/?project_url=https://github.com/GaiaMod-Main/examples/raw/refs/heads/main/src/gaia-files/ChatGPT.gaia"
+        href: "https://gaiamod-main.github.io/?project_url=https://raw.githubusercontent.com/GaiaMod-Main/examples/refs/heads/main/src/gaia-files/ChatGPT.gaia"
     }
 ]
 
